73c51eefd461ba7d2dbe7ad43b2fc0e666773285
Nqtsu91 <55384628+Nqtsu91@users.noreply.github.com>
2026-07-18 16:23:03 +0200

    fix: breeze rods spin constantly, whatever animation is playing

--- src/main/java/com/natsu/backport/common/entity/model/BreezeModel.java
+++ src/main/java/com/natsu/backport/common/entity/model/BreezeModel.java
@@ -4,9 +4,24 @@ import com.natsu.backport.CTBackport;
 import com.natsu.backport.common.entity.Breeze;
 
 import net.minecraft.resources.ResourceLocation;
+import software.bernie.geckolib3.core.event.predicate.AnimationEvent;
+import software.bernie.geckolib3.core.processor.IBone;
 import software.bernie.geckolib3.model.AnimatedGeoModel;
 
 public class BreezeModel extends AnimatedGeoModel<Breeze> {
+
+	// full turn every ~2s, whatever animation is playing
+	private static final float RODS_SPIN_SPEED = 0.15F;
+
+	@Override
+	public void setCustomAnimations(Breeze breeze, int instanceId, AnimationEvent animationEvent) {
+		super.setCustomAnimations(breeze, instanceId, animationEvent);
+		IBone rods = this.getAnimationProcessor().getBone("rods");
+		if (rods != null) {
+			float partialTick = animationEvent == null ? 0.0F : (float) animationEvent.getPartialTick();
+			rods.setRotationY((breeze.tickCount + partialTick) * RODS_SPIN_SPEED);
+		}
+	}
 
     @Override
     public ResourceLocation getModelLocation(Breeze entity) {
